31543595ed68f08ec3033dd263296af1d9325c98
yosufali <yosufali@users.noreply.github.com>
2015-11-25 20:31:22 +0000

    Added connection to database, AND converted time for each selected expiration into seconds

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php	
+
+	// connecting to the database
+	$db = new mysqli('', '', '', '');
+	// echo '<pre>', var_dump($db), '</pre>';
+
+
+	//message for users if there are issues with the server
+	if ($db -> connect_errno !== 0) {
+		die('We are down at the moment, please try again soon.');
+	}
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+//--------------------------------------------------------------------------------------
+
 	//if there are items added, validate them before uploading them
 	if (isset($_FILES['item'])) {
 		$errors = array();
@@ -22,7 +24,7 @@
 
 		//Upload files if there are no errors, otherwise output errors
 		if (empty($errors)) {
-			if (move_uploaded_file($file_tmp, 'images/'.$file_name)) {
+			if (move_uploaded_file($file_tmp, 'files/'.$file_name)) {
 				echo 'File uploaded';
 			}
 		} else {
@@ -32,16 +34,59 @@
 		}
 	}
 
+//--------------------------------------------------------------------------------------
+
 	// get the selected expiration time from the dropdown (select) and store it
+	// $time is the time after which the file will be deleted
 	if (isset($_POST['time'])) {
 		$selectOption = $_POST['time'];
 		print_r($selectOption);
+
+		switch ($selectOption) {
+			case '5m':
+				$time = 300;
+				break;
+			case '1h':
+				$time = 3600;
+				break;
+			case '6h':
+				$time = 21600;
+				break;
+			case '24hr':
+				$time = 86400;
+				break;
+			case '1w':
+				$time = 604800;
+				break;
+			case '2w':
+				$time = 1209600;
+				break;
+			case '1m':
+				$time = 2592000;
+				break;
+			default:
+				//will never be default
+		}
 	}
 
+//--------------------------------------------------------------------------------------
+
+	//store password in a variable
 	if (isset($_POST['password'])) {
 		$password = $_POST['password'];
 		print_r($password);
 	}
+
+//--------------------------------------------------------------------------------------
+	//print current time - delete after
+	print_r(time());
+
+//--------------------------------------------------------------------------------------
+	//connect to database
+	include 'db.php';
+	
+//--------------------------------------------------------------------------------------
+
 ?>
 
 <form action="" method="POST" enctype="multipart/form-data">
